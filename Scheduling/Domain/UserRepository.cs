939e977dca67d491269cba3f2f1c3a958258fe48
Illya-Eismont <73027760+Illya-Eismont@users.noreply.github.com>
2021-04-11 01:37:58 +0300

    update back-end

---
+++ Scheduling/Domain/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using Scheduling.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduling.Domain
+{
+    interface IUserRepository
+    {
+        IEnumerable<User> Get();
+        User Get(string email);
+
+        List<Permission> GetPermission(string email);
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        readonly UserDBContext Context;
+
+        public UserRepository(UserDBContext context)
+        {
+            Context = context;
+        }
+
+        public IEnumerable<User> Get()
+        {
+            return Context.Users;
+        }
+
+        public User Get(string email)
+        {
+            return Context.Users.Single(user => user.Email == email);
+        }
+
+        public List<Permission> GetPermission(string email)
+        {
+            User user = Context.Users.Single(user => user.Email == email);
+            List<UserPermission> userPermissions = Context.UserPermissions.Where(permission => permission.UserId == user.Id).ToList<UserPermission>();
+            List<Permission> permissions = new List<Permission>();
+
+            foreach (UserPermission userPermission in userPermissions)
+            {
+                permissions.Add(Context.Permissions.Single(permission => permission.Id == userPermission.PermisionId));
+            }
+            return permissions;
+        }
+    }
+}
---
+++ Scheduling/GraphQl/Schemas/LoginSchema.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using Microsoft.Extensions.DependencyInjection;
+using Scheduling.GraphQl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduling.GraphQl.Schemas
+{
+    public class LoginSchema : Schema
+    {
+        public LoginSchema(IServiceProvider provider) : base(provider)
+        {
+            Query = provider.GetRequiredService<Querys>();
+            Mutation = provider.GetRequiredService<Mutations>();
+        }
+    }
+}
