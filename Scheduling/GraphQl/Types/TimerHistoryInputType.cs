e2f93773dd8ec47e26d7df544b4f2fbacab3880c
barii64 <venedchukmssha@gmail.com>
2021-04-20 16:25:39 +0300

    0

---
+++ Scheduling/GraphQl/Types/TimerHistoryInputType.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduling.GraphQl.Types
+{
+    public class TimerHistoryInputType : InputObjectGraphType
+    {
+        public TimerHistoryInputType()
+        {
+            Name = "TimerHistoryInputType";
+            Field<NonNullGraphType<IntGraphType>>("Id");
+            Field<NonNullGraphType<DateGraphType>>("StartTime");
+            Field<NonNullGraphType<DateGraphType>>("FinishTime");
+        }
+    }
+}
