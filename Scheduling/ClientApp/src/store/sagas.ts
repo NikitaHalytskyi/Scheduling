d4f917a1471a79872f09cbf8e2c5268c321136a6
Vladyslav <ipz201_svo@student.ztu.edu.ua>
2021-04-22 01:03:24 +0300

    added root saga

--- Scheduling/ClientApp/src/store/sagas.ts
+++ Scheduling/ClientApp/src/store/sagas.ts
@@ -1,7 +1,8 @@
 import { put, takeEvery, all } from 'redux-saga/effects'
+import getUserDataSaga from './UserManagement/sagas'
 
 export default function* rootSaga() {
     yield all([
-
+        getUserDataSaga(),
     ])
 }
