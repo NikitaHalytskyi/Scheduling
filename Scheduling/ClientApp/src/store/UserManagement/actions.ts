17b91066c6c186ecb02d50dda12c513f0cfe01c4
Vladyslav <ipz201_svo@student.ztu.edu.ua>
2021-04-22 01:03:00 +0300

    actions updating
    Added requested and received users actions

--- Scheduling/ClientApp/src/store/UserManagement/actions.ts
+++ Scheduling/ClientApp/src/store/UserManagement/actions.ts
@@ -1,21 +1,25 @@
 import { UserData } from "../User/types";
 
 
-export interface SetUsersAction { type: 'SET_USERS', payload: Array<UserData>};
-export interface CheckPermissions { type: 'CHECK_PERMISSIONS'};
-export interface AccessAllowed { type: 'ACCESS_ALLOWED'};
-export interface AccessDenied {type: 'ACCESS_DENIED'};
+export interface ReceivedUsersDataAction { type: 'RECEIVED_USERS', payload: Array<UserData> };
+export interface RequestedUsersDataAction { type: 'REQUESTED_USERS' };
+export interface CheckPermissions { type: 'CHECK_PERMISSIONS' };
+export interface AccessAllowed { type: 'ACCESS_ALLOWED' };
+export interface AccessDenied { type: 'ACCESS_DENIED' };
 
-const setUsers = (users: Array<UserData>) => ({type: 'SET_USERS', payload: users} as SetUsersAction);
-const checkPermissions = () => ({type: 'CHECK_PERMISSIONS'} as CheckPermissions);
-const accessAllowed = () => ({type: 'ACCESS_ALLOWED'} as AccessAllowed);
-const accessDenied = () => ({type: 'ACCESS_DENIED'} as AccessDenied);
+const receivedUsersData = (users: Array<UserData>) => ({ type: 'RECEIVED_USERS', payload: users } as ReceivedUsersDataAction);
+const requestedUsersData = () => ({ type: 'REQUESTED_USERS' } as RequestedUsersDataAction);
+const checkPermissions = () => ({ type: 'CHECK_PERMISSIONS' } as CheckPermissions);
+const accessAllowed = () => ({ type: 'ACCESS_ALLOWED' } as AccessAllowed);
+const accessDenied = () => ({ type: 'ACCESS_DENIED' } as AccessDenied);
 
 export const actionCreators = {
-    setUsers,
+    receivedUsersData,
+    requestedUsersData,
     checkPermissions,
     accessAllowed,
     accessDenied
 };
 
-export type KnownAction = SetUsersAction | CheckPermissions;
+export type KnownAction = ReceivedUsersDataAction | CheckPermissions |
+    RequestedUsersDataAction | AccessAllowed | AccessDenied;
